7823acf863206961cca60cf5122f7b5827cfb855
imane ELKADI <imaneelkadi22@gmail.com>
2023-02-12 00:39:16 +0100

    ajouter in index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<fieldset style='border-color:black; width:150px; background-color:pink;'>
+<form action='notes.php' method='post'>
+<table>
+<tr>
+<td> <label for='user'> User :</label></td>
+<td> <input style='border-color:red;' type='text' name='user' ></td>
+</tr>
+<tr>
+<td> <label for='psw'> Login</label></td>
+<td> <input style='border-color:purple;' type='text' name='psw' ></td>
+</tr>
+
+<tr>
+<td> <input  style='border-color:pink;' type='submit' name='valider' value='valider'></td>
+</tr>
+</table>
+</form>
+</fieldset>
