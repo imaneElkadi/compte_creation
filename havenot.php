43ccc707d906d09a59218ecbe709f33a1b1c44d0
imane ELKADI <imaneelkadi22@gmail.com>
2023-02-12 01:19:35 +0100

    add new changes

---
+++ havenot.php
@@ -0,0 +1,32 @@
+<h1>Create New Account</h1>
+<fieldset style='border-color:black; width:150px; background-color:pink;'>
+<form action='n.php' method='post'>
+<table>
+<tr>
+<td> <label for='name'> Name :</label></td>
+<td> <input style='border-color:red;' type='text' name='name' ></td>
+</tr>
+<tr>
+<td> <label for='lastname'> Last Name :</label></td>
+<td> <input style='border-color:red;' type='text' name='lastname' ></td>
+</tr>
+<tr>
+<td> <label for='email'> Email :</label></td>
+<td> <input style='border-color:red;' type='email' name='email' ></td>
+</tr>
+<tr>
+<td> <label for='username'> User Name :</label></td>
+<td> <input style='border-color:red;' type='text' name='username' ></td>
+</tr>
+<tr>
+<td> <label for='psw'> New Password</label></td>
+<td> <input style='border-color:purple;' type='password' name='psw' ></td>
+</tr>
+  
+<tr>
+
+<td> <input  style='border-color:pink;' type='submit' name='valider' value='valider'></td>
+</tr>
+</table>
+</form>
+</fieldset>
--- index.php
+++ index.php
@@ -1,18 +1,19 @@
+<h1>Welcome</h1>
 <fieldset style='border-color:black; width:150px; background-color:pink;'>
-<form action='notes.php' method='post'>
+<form action='have.php' method='post'>
 <table>
 <tr>
-<td> <label for='user'> User :</label></td>
-<td> <input style='border-color:red;' type='text' name='user' ></td>
+ <td><input style='border-color:pink;' type="submit" value="have account"></td>
 </tr>
+</table>
+</form>
+</fieldset>
+<br>
+<fieldset style='border-color:black; width:150px; background-color:pink;'>
+<form action='havenot.php' method='post'>
+<table>
 <tr>
-<td> <label for='psw'> Login</label></td>
-<td> <input style='border-color:purple;' type='text' name='psw' ></td>
-</tr>
-  
-<tr>
-
-<td> <input  style='border-color:pink;' type='submit' name='valider' value='valider'></td>
+ <td><input  style='border-color:purple;' type="submit" value="have not an account"></td>
 </tr>
 </table>
 </form>
